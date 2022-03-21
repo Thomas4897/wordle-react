fa83c51e7a55e0a511cd4b4b679912b494120210
Thomas Maynard <90271276+Thomas4897@users.noreply.github.com>
2022-03-22 00:31:12 +0800

    Installing React

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./App.css";
+
+export class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Wordle</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
